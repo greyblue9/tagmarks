6fc4055c4aa6beaa95842259563f31eb3d2e4cb0
David Reilly <greyblue9@gmail.com>
2014-04-19 14:31:55 -0400

    - Big refactor, split up classes into their own files in NS folder - Methods generally well-documented, new-style syntax, class autoloading - Things work again - Encapsulated site/tag/state storage/retrieval into DataSource.class.inc in preparation for move to SQLite3, then to multi-user environment

--- data.php
+++ data.php
@@ -2,146 +2,71 @@
 
 namespace Tagmarks;
 
-require('include/common.inc.php');
-
-
-Setup::readIniFiles();
-
-
-$tldData = array();
-$columnHeaders = array();
-$csv = fopen(TLDS_LIST_FILENAME, "r");
-if ($csv !== FALSE) {
-	$rowIdx = 0;
-	while (($row = fgetcsv($csv, 1000, ',', '"', '"')) !== FALSE) {
-
-		if ($rowIdx === 0) {
-			$columnHeaders = $row;
-		} else {
-			$tldInfo = array();
-			foreach ($row as $idx => $val) {
-				$val = preg_replace('/\p{Cc}+/u', '', $val);
-				$tldInfo[$columnHeaders[$idx]] = $val;
-			}
-
-			$tldData[] = $tldInfo;
-		}
-
-		$rowIdx++;
-	}
-	fclose($csv);
-}
+require_once('include/common.inc');
+require_once('include/Tagmarks/common.inc');
 
 
 
+$setup = new Setup(
+	WEB_ROOT.'/defaults', // defaults dir
+	TAGMARKS_PRIVATE_DIR // private data dir
+);
+$dataSource = new DataSource(
+	TAGMARKS_PRIVATE_DIR.'/data.json'
+);
 
-function saveSites($sites)
-{
-	$mainDataJson = file_get_contents(MAIN_DATA_FILEPATH);
-	$mainData = Json::decodeOrOutputError($mainDataJson, 'json');
-
-	$mainData['sites'] = $sites;
-	$mainDataJson = json_encode($mainData, JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES);
-
-	$bytesWrittenOrFalse = file_put_contents(MAIN_DATA_FILEPATH, Json::formatJson($mainDataJson));
-	$saveSucceeded =
-		$bytesWrittenOrFalse !== false &&
-		$bytesWrittenOrFalse === strlen($mainDataJson);
-
-	return array(
-		'server_action' => 'save_sites',
-		'site_count' => count($sites),
-		'save_file' => MAIN_DATA_FILEPATH,
-		'save_file_realpath' => realpath(MAIN_DATA_FILEPATH),
-		'save_bytes_attempted' => strlen($mainDataJson),
-		'save_bytes_written' => intval($bytesWrittenOrFalse),
-		'save_failure' => $bytesWrittenOrFalse === false,
-		'save_succeeded' => $saveSucceeded,
-		'result' => $saveSucceeded
-	);
-
-}
 
 $method = $_SERVER['REQUEST_METHOD'];
-$responseArray = array();
+switch ($method) {
 
-if ($method == 'POST') {
-	$postData = json_decode(file_get_contents('php://input'), true);
+	case 'GET':
+		$secureVars = $setup->getSecureVars();
+		$mainData = $dataSource->getMainData($secureVars);
+		$tldData = $setup->getTldData();
 
-	if (isset($postData['sites'])) {
-		$responseArray = saveSites($postData['sites']);
-	} else {
-		$responseArray = array(
-			'error' => true,
-			'errorText' => 'Unsupported post context',
-			'postData' => $postData
-		);
-	}
-
-} else if ($method == 'GET') {
-
-	// load datafile
-	if (!file_exists(MAIN_DATA_FILEPATH)) {
-		Errors::exitWithFatalError(
-			'Expecting '.MAIN_DATA_FILEPATH.' relative to script directory.');
-	}
-
-	$main_data_file_contents = file_get_contents(MAIN_DATA_FILEPATH);
-	$main_data = Json::decodeOrOutputError($main_data_file_contents, 'json');
+		$responseArray = [
+			'data' => $mainData,
+			'tlds' => $tldData
+		];
+		break;
 
 
-	$sites = & $main_data['sites'];
-	$sitesChanged = false;
-	foreach ($sites as &$site) {
-		if (!isset($site['id'])) {
-			$site['id'] = mt_rand(10000000, 99999999);
-			$sitesChanged = true;
+	case 'POST':
+		$postBody = file_get_contents('php://input');
+		$postData = Json::decode($postBody, true);
+
+		if (isset($postData['sites'])) {
+			$saveResult = $dataSource->saveSites($postData['sites']);
+			$responseArray = $saveResult;
+		} else {
+			$responseArray = [
+				'error' => true,
+				'errorText' => 'Unsupported post context',
+				'postData' => $postData
+			];
 		}
-	}
-	if ($sitesChanged) {
-		file_put_contents(MAIN_DATA_FILEPATH, Json::formatJson(json_encode($main_data, JSON_NUMERIC_CHECK)));
-	}
+		break;
 
 
-	$secure_vars = Setup::getSecureVars(SECURE_DATA_SCRIPT_FILENAME);
-
-	if ($secure_vars) {
-		// The variable $data_with_secvars will contain any secure data in plaintext
-		$data = Common::getDeepVariableCopy($main_data);
-		Setup::replaceSecureVars($data, $secure_vars);
-	}
-	else {
-		$data = $main_data;
-	}
-
-	$responseArray = array(
-		'data' => $data,
-		'tlds' => $tldData
-	);
-
-
-} else {
-	// Unsupported HTTP request method
-	$responseArray = array(
-		'error' => true,
-		'errorCode' => 9,
-		'errorText' => 'Unsupported request method',
-		'requestMethod' => $method
-	);
+	default:
+		// Unsupported HTTP request method
+		$responseArray = [
+			'error' => true,
+			'errorCode' => 9,
+			'errorText' => 'Unsupported request method',
+			'requestMethod' => $method
+		];
 }
 
 
 
+header('Content-Type: application/json;charset=UTF-8');
 
-header('Content-Type: application/json; charset=utf-8');
+$outputJson = Json::encode($responseArray);
+print($outputJson);
+exit();
 
-$jsonEncodeStr = json_encode($responseArray, JSON_NUMERIC_CHECK | JSON_UNESCAPED_SLASHES);
 
-$output = JSON_INDENTED_OUTPUT?
-	Json::formatJson($jsonEncodeStr):
-	$jsonEncodeStr;
-
-print($output);
 
 
 
